ee33767696d74132042921180db488a3b9d6ae37
ishan <ishan@DESKTOP-LDOG3I6>
2020-04-14 02:03:49 +1000

    Add project files.

---
+++ HomeWildLearn/Controllers/HomeController.cs
@@ -0,0 +1,74 @@
+﻿using HomeWildLearn.Models;
+using HomeWildLearn.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HomeWildLearn.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View(new SendEmailViewModel());
+        }
+
+        [HttpPost]
+        public ActionResult About(SendEmailViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    String toEmail = model.ToEmail;
+                    String subject = model.Subject;
+                    String contents = model.Contents;
+
+                    EmailSender es = new EmailSender();
+                    es.Send(toEmail, subject, contents);
+
+                    ViewBag.Result = "Email has been send.";
+
+                    ModelState.Clear();
+
+                    return View(new SendEmailViewModel());
+                }
+                catch
+                {
+                    return View();
+                }
+            }
+
+            return View();
+        }
+
+        public ActionResult Kids()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+        public ActionResult Parents()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+        public ActionResult Whatsnew()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+    }
+}
---
+++ HomeWildLearn/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HomeWildLearn.MvcApplication" Language="C#" %>
